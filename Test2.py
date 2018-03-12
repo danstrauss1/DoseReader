bc05ed94d16f7abe702d1381d2b3cec5837994ef
Dan-123 <Dan-123@users.noreply.github.com>
2018-03-12 00:16:40 -0700

    Test2.py Initial Commit

--- Test2.py
+++ Test2.py
@@ -4,8 +4,8 @@ dose = pydicom.read_file('DoseVolumeTest.dcm')
 d = np.fromstring(dose.PixelData, dtype=np.int16)
 #d = d.reshape((dose.NumberOfFrames, dose.Columns, dose.Rows))
 
-xgrid = np.arange(0, dose.Columns, dose.PixelSpacing[0])
-ygrid = np.arange(0, dose.Rows, dose.PixelSpacing[1])
-zgrid = np.arange(0, dose.NumberOfFrames, int(dose.GridFrameOffsetVector[1-0]))
+xgrid = np.arange(dose.ImagePositionPatient[0], dose.Columns, dose.PixelSpacing[0])
+ygrid = np.arange(dose.ImagePositionPatient[1], dose.Rows, dose.PixelSpacing[1])
+zgrid = np.arange(dose.ImagePositionPatient[2], dose.NumberOfFrames, int(dose.GridFrameOffsetVector[1-0]))
 grad = np.gradient(d)
 
